0da7e2d2133b7328f3b249e5e73092f4a1397a32
kbrdyms <77927356+kbrdyms@users.noreply.github.com>
2021-03-09 00:17:01 +0300

    Add files via upload

---
+++ Arduino Problem Seti 2/Trafik_Isiklari.ino
@@ -0,0 +1,23 @@
+
+void setup()
+{
+  pinMode(4, OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(2,OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(4, HIGH);
+  delay(10000); 
+  digitalWrite(3,HIGH);
+  delay(10000);
+  digitalWrite(4, LOW);
+  delay(2000);
+  digitalWrite(3,LOW);
+  digitalWrite(2,HIGH);
+  delay(5000);
+  digitalWrite(2,LOW);
+  
+  
+}
